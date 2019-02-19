e9c2951c6a0dcfe71ccfdd72937bd493f1549f4e
Eben Blaisdell <emblaisdell@foresthills.net>
2019-02-18 21:52:07 -0500

    add memes and script

---
+++ eweek/script.js
@@ -0,0 +1,30 @@
+var memes = [
+  {
+    pos:[-2,0],
+    name="meme00.jpg"
+  },
+  {
+    pos:[2,0],
+    name="meme01.jpg"
+  }
+];
+
+function addImage(image){
+  var position = image.pos.x+" "+image.pos.y+" 0";
+  $('<a-image />', {
+    id: name,
+    class: "",
+    position: position,  // doesn't seem to do anything, known issue
+    scale: "1 1 1",
+    rotation: "-90 0 0",
+    src: image.name,
+    appendTo : $('#hiro-marker')
+  });
+  document.getElementById(id).setAttribute("position", position); // this does set position as a workaround
+}
+
+$(function(){
+  memes.each(function(meme){
+    console.log(meme);
+  })
+});
