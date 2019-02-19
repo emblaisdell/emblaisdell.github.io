51d87b667d9bdf45db9d28c5385c46aa18815054
Eben Blaisdell <emblaisdell@foresthills.net>
2019-02-18 22:19:16 -0500

    publish memes to banner

--- eweek/script.js
+++ eweek/script.js
@@ -13,13 +13,23 @@ function addImage(image){
   var position = image.pos[0]+" "+image.pos[1]+" 0";
   console.log(position);
   $('<a-image />', {
-    id: image.name,
+    id: image.name+"_hiro",
     class: "",
     position: position,  // doesn't seem to do anything, known issue
     scale: "4 4 4",
     rotation: "-90 0 0",
     src: image.name,
     appendTo : $('#hiro-marker')
+  });
+  document.getElementById(image.name+"_hiro").setAttribute("position", position); // this does set position as a workaround
+  $('<a-image />', {
+    id: image.name,
+    class: "",
+    position: position,  // doesn't seem to do anything, known issue
+    scale: "4 4 4",
+    rotation: "-90 0 0",
+    src: image.name,
+    appendTo : $('#banner-marker')
   });
   document.getElementById(image.name).setAttribute("position", position); // this does set position as a workaround
 }
