5b69fd0eb0da0179e5fd3372576b5379e921180f
emblaisdell <emblaisdell@foresthills.net>
2024-06-25 11:19:27 -0400

    add list support

--- ontolog/ace/types.js
+++ ontolog/ace/types.js
@@ -26,4 +26,33 @@ export class ACEBuffer {
         const view = new DataView(this.uint8Array.buffer);
         return view.getInt32(0, false); // true for little-endian
     }
+
+    static fromList(list) {
+        const buffers = list.map(e => e.uint8Array)
+        const newSize = buffers.map(e => e.length).sum() + 4 * buffers.length
+        const out = new Uint8Array(newSize)
+        const view = new DataView(out);
+        var pos = 0
+        buffers.forEach(buf => {
+            view.setInt32(pos, buf.length, false)
+            out.set(buf, pos + 4)
+            pos += 4 + buf.length
+        });
+        return new ACEBuffer(out)
+    }
+
+    toList() {
+        var pos = 0
+        var out = []
+        const view = new DataView(this.uint8Array)
+        while (pos < this.uint8Array.length) {
+            const len = view.getInt32(pos, false)
+            out.push(new ACEBuffer(
+                this.uint8Array.slice(pos+4, pos+4+len))
+            )
+            pos += 4 + len
+        }
+        return out
+    }
 }
+
